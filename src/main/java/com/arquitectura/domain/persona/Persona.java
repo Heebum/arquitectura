b15717f37aa5b700c512bd299090631f27d6b0b4
Stefanie <stefanie_0627@hotmail.com>
2022-04-11 11:50:43 -0500

    arquitectura

---
+++ src/main/java/com/arquitectura/domain/persona/Persona.java
@@ -0,0 +1,65 @@
+package com.arquitectura.domain.persona;
+
+import java.util.Objects;
+
+public class Persona {
+
+    private String codigo;
+    private String identificacion;
+    private String nombre;
+    private String apellido;
+    private String edad;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Persona persona = (Persona) o;
+        return Objects.equals(codigo, persona.codigo) && Objects.equals(identificacion, persona.identificacion) && Objects.equals(nombre, persona.nombre) && Objects.equals(apellido, persona.apellido) && Objects.equals(edad, persona.edad);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigo, identificacion, nombre, apellido, edad);
+    }
+}
